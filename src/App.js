ca8264e5a4aede73264c66f6ae5cf85e54751dbb
TecFelipeDsMartins <tec.felipedsmartins@gmail.com>
2020-09-28 08:30:58 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from './Components/Card'
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <Card/>
+    </div>
+  );
+}
